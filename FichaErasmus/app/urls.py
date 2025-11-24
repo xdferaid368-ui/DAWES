da9e5e40b950da6716ebd561abc0e623fcc2d81b
xdferaid368-ui <xdferaid368@ieshnosmachado.org>
2025-11-24 13:38:07 +0100

    detalle_alumno

--- FichaErasmus/app/urls.py
+++ FichaErasmus/app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.principal, name='principal'),
+    path('ficha/<int:id>/', views.detalle_alumno, name='detalle_alumno'),
+
 ]
--- FichaErasmus/app/views.py
+++ FichaErasmus/app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 # Create your views here.
 
 def principal(request):
     alumnos = Alumno.objects.all().order_by('apellidos', 'nombre')
     return render(request, 'app/principal.html', {'alumnos': alumnos})
+
+def detalle_alumno(request, id):
+    alumno = get_object_or_404(Alumno, id=id)
+    return render(request, 'app/detalle_alumno.html', {'alumno': alumno})
